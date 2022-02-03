cc2062f91229ad6319a0f2d75d9865ae23734ca5
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-03 16:41:51 -0500

    feat: Create `Lengths.java`

---
+++ JavaBasics/Lengths.java
@@ -0,0 +1,12 @@
+
+public class Lengths {
+
+	public static void main(String[] args) {
+
+		// Escape
+		System.out.println("\\".length()); // Prints 1
+		System.out.println("\"".length()); // Prints 1
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nayeons-java-lab
+
+- `Lengths.java`: Escape characters don't count towards length.
