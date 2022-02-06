030d8c7bd11f94eb69fbb45f71068baed038e026
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-05 19:28:06 -0500

    feat(JavaBasics): Create `Equality.java`

---
+++ JavaBasics/Equality.java
@@ -0,0 +1,15 @@
+
+public class Equality {
+
+	public static void main(String[] args) {
+
+		String myName = "nayeon";
+		String myName2 = new String("nayeon");
+		String myName3 = "nayeon";
+
+		System.out.println(myName == myName2); // Prints false
+		System.out.println(myName.equals(myName2)); // Prints true
+		System.out.println(myName == myName3); // Prints true
+	}
+
+}
